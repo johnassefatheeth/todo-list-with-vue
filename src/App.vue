3b6cecaf1fd7a6cba8654348be173838dfc426d7
johnassefatheeth <johnassefaforbussines@gmail.com>
2023-08-08 05:32:42 -0700

    setting up the script section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <script setup>
+import { ref,onMounted, computed , watch} from 'vue'
+
+const todos= ref([])
+const name =ref('')
+
+const input_content= ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(()=> todos.value.sort((a,b)=>{
+  return b.createdAt = a.createdAt
+}))
+
+
 </script>
 
 <template>
-  
+<!-- <main class="app">
+  <section class="greeting">
+    <h2 class="title">
+      hello there, <input type="text" placeholder="your name here" v-model="name">
+    </h2>
+  </section>
+</main> -->
 </template>
 
